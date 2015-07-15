04b2ef40e3b8782cd76774df0691a81455258d97
kskovpen <Kirill.Skovpen@cern.ch>
2015-07-15 23:39:48 +0200

    add JSON file

--- FlatTreeProducer/test/PROD/crabConfigTemplate.py
+++ FlatTreeProducer/test/PROD/crabConfigTemplate.py
@@ -6,12 +6,12 @@ config.General.requestName = 'REQUESTNAME'
 config.section_('JobType')
 config.JobType.psetName = '../runFlatTreeMINIAOD_cfg.py'
 config.JobType.pluginName = 'Analysis'
-config.JobType.inputFiles = ['../conf.xml']
+config.JobType.inputFiles = ['../conf.xml','../PHYS14_V4_MC.db']
 #config.JobType.outputFiles = ['output.root']
 #config.JobType.pyCfgParams = ['isData=0']
 config.section_('Data')
-#config.Data.totalUnits = 5
-config.Data.totalUnits = 10
+config.Data.totalUnits = 1
+#config.Data.totalUnits = 10
 #config.Data.unitsPerJob = 1
 config.Data.unitsPerJob = 2
 config.Data.splitting = 'FileBased'
@@ -20,6 +20,7 @@ config.Data.inputDataset = 'INPUTDATASET'
 config.Data.publishDataName = 'PUBLISHDATANAME'
 config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter'
 config.Data.outLFNDirBase = 'OUTLFN'
+config.Data.lumiMask = 'GRL/json_DCSONLY_Run2015B.txt'
 config.section_('User')
 config.section_('Site')
 config.Site.storageSite = 'T2_FR_IPHC'
