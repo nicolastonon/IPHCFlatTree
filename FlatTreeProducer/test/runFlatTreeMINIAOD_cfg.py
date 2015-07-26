3c27b50c807589077150a650a23c104ed5b2ba1a
kskovpen <Kirill.Skovpen@cern.ch>
2015-07-26 19:21:39 +0200

    7_4_7 update

--- FlatTreeProducer/test/runFlatTreeMINIAOD_cfg.py
+++ FlatTreeProducer/test/runFlatTreeMINIAOD_cfg.py
@@ -1,3 +1,3 @@
 from IPHCFlatTree.FlatTreeProducer.ConfFile_MINIAOD_cfg import *
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
-#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+#process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
