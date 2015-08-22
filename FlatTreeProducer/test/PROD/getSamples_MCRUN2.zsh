10946e3ea7d23d9c3c0b389b36136fa579d4b60c
kskovpen <Kirill.Skovpen@cern.ch>
2015-08-22 10:23:43 +0200

    Run2 update

---
+++ FlatTreeProducer/test/PROD/getSamples_MCRUN2.zsh
@@ -0,0 +1,8 @@
+#!/bin/env zsh
+
+#tag=RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A_ # (50ns)
+tag=RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ # (25ns)
+
+wget --no-check-certificate \
+--output-document=samples_MCRUN2.txt \
+"https://cmsweb.cern.ch/das/request?view=plain&limit=1000&instance=prod%2Fglobal&input=dataset%3D%2F*%2F*${tag}*%2FMINIAODSIM+|+sort+dataset.name"
