9b1705ac0c1aaad5ff02106c921b2c98abc398ba
kskovpen <Kirill.Skovpen@cern.ch>
2015-08-22 10:23:34 +0200

    Run2 update

--- FlatTreeProducer/test/PROD/crabConfigTemplate.py
+++ FlatTreeProducer/test/PROD/crabConfigTemplate.py
@@ -6,21 +6,22 @@ config.General.requestName = 'REQUESTNAME'
 config.section_('JobType')
 config.JobType.psetName = '../runFlatTreeMINIAOD_cfg.py'
 config.JobType.pluginName = 'Analysis'
-config.JobType.inputFiles = ['../conf.xml','../PHYS14_V4_MC.db']
+config.JobType.inputFiles = ['../conf.xml','../Summer15_50nsV2_MC.db']
 #config.JobType.outputFiles = ['output.root']
-#config.JobType.pyCfgParams = ['isData=0']
+config.JobType.pyCfgParams = ['isData=0','applyJEC=1']
 config.section_('Data')
 config.Data.totalUnits = 1
 #config.Data.totalUnits = 10
-#config.Data.unitsPerJob = 1
-config.Data.unitsPerJob = 2
-config.Data.splitting = 'FileBased'
+#config.Data.unitsPerJob = 2
+config.Data.unitsPerJob = 20
+#config.Data.splitting = 'FileBased'
+config.Data.splitting = 'LumiBased'
 config.Data.publication = False
 config.Data.inputDataset = 'INPUTDATASET'
 config.Data.publishDataName = 'PUBLISHDATANAME'
 config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter'
 config.Data.outLFNDirBase = 'OUTLFN'
-config.Data.lumiMask = 'GRL/json_DCSONLY_Run2015B.txt'
+config.Data.lumiMask = 'GRL/Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt' 
 config.section_('User')
 config.section_('Site')
 config.Site.storageSite = 'T2_FR_IPHC'
