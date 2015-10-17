72c2bbbd9b0e8d570526dceb4a72d00ac418dfda
kskovpen <Kirill.Skovpen@cern.ch>
2015-10-17 16:43:57 +0200

    update lists to latest prod tags

--- FlatTreeProducer/test/PROD/getSamples_MCRUN2.zsh
+++ FlatTreeProducer/test/PROD/getSamples_MCRUN2.zsh
@@ -1,7 +1,7 @@
 #!/bin/env zsh
 
 #tag=RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A_ # (50ns)
-tag=RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_ # (25ns)
+tag=RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2 # (25ns)
 
 wget --no-check-certificate \
 --output-document=samples_MCRUN2.txt \
