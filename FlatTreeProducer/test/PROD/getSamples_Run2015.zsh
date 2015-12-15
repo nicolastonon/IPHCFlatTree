674e37e1002ecd374822c6c6f445e2c7c0e1558b
kskovpen <Kirill.Skovpen@cern.ch>
2015-12-15 14:24:10 +0100

    update

--- FlatTreeProducer/test/PROD/getSamples_Run2015.zsh
+++ FlatTreeProducer/test/PROD/getSamples_Run2015.zsh
@@ -2,4 +2,4 @@
 
 wget --no-check-certificate \
 --output-document=samples_Run2015.txt \
-"https://cmsweb.cern.ch/das/request?view=plain&limit=1000&instance=prod%2Fglobal&input=dataset%3D%2F*%2F*Run2015*05Oct2015*%2FMINIAOD+|+sort+dataset.name"
+"https://cmsweb.cern.ch/das/request?view=plain&instance=prod%2Fglobal&input=dataset%3D%2F*%2F*Run2015*%2FMINIAOD+|+sort+dataset.name"
