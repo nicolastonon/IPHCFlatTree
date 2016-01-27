3979a4ebf0055ce70fb64ed74e9a0406bbfbb775
kskovpen <Kirill.Skovpen@cern.ch>
2016-01-27 20:40:21 +0100

    jetNDauChargedMVASel added

--- FlatTreeProducer/interface/Helper.hh
+++ FlatTreeProducer/interface/Helper.hh
@@ -48,7 +48,6 @@ double getPFIsolation(edm::Handle<pat::PackedCandidateCollection>,
 		      
 bool qualityTrk(const reco::Track trk, const reco::Vertex &vtx);
 
-int jetNDauChargedMVASel(const pat::Muon& muon, const pat::Jet& jet);
-int jetNDauChargedMVASel(const pat::Electron& elec, const pat::Jet& jet);
+int jetNDauChargedMVASel(const pat::Jet& jet, const reco::Vertex& vtx);
 
 #endif
