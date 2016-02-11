d18ec990ea9ba066973c82bf28bce99778932d71
kskovpen <Kirill.Skovpen@cern.ch>
2016-02-11 09:12:12 +0100

    update tags

--- FlatTreeProducer/test/PROD/getSamples_MCRUN2.zsh
+++ FlatTreeProducer/test/PROD/getSamples_MCRUN2.zsh
@@ -1,6 +1,6 @@
 #!/bin/env zsh
 
-tag=RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2 # (25ns)
+tag=RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12
 
 wget --no-check-certificate \
 --output-document=samples_MCRUN2.txt \
