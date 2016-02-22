38e7dc09fd33ac0a66c144bbd9cb9986668364eb
kskovpen <Kirill.Skovpen@cern.ch>
2016-02-22 11:28:00 +0100

    update submit script

--- FlatTreeProducer/test/PROD/submit.zsh
+++ FlatTreeProducer/test/PROD/submit.zsh
@@ -2,7 +2,7 @@
 
 slist="list.txt"
 pset="crabConfigTemplate.py"
-ver="Akoula-patch3-v20160204"
+ver="Akoula-patch4-v20160222"
 prodv="/store/user/kskovpen/FlatTree/${ver}/"
 
 rm -f crabConfig.py*
@@ -20,7 +20,7 @@ do
   spl=($(echo $i | tr "/" "\n"))
   pubdn=$(echo "${spl[2]}_${spl[3]}" | sed 's%-%_%g')
   nam=$(echo "${spl[1]}" | sed 's%-%_%g')
-  reqn=$(echo "${nam}_${pubdn}" | sed 's%_RunIIFall15MiniAODv2_PU25nsData2015v1_76X_mcRun2_asymptotic_v12_v1%%g' | sed 's%AODFASTSIM.*%AODFASTSIM%g')
+  reqn=$(echo "${nam}_${pubdn}" | sed 's%_RunIIFall15MiniAODv2_PU25nsData2015v1_76X_mcRun2_asymptotic_v12_%%g' | sed 's%AODFASTSIM.*%AODFASTSIM%g')
   cat ${pset} | sed "s%INPUTDATASET%${i}%g" \
   | sed "s%OUTLFN%${prodv}%g" \
   | sed "s%REQUESTNAME%${reqn}%g" \
