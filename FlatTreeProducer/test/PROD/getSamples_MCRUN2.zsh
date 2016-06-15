bfae10d406e71f9ef79d337edec77e8d52a9e118
kskovpen <Kirill.Skovpen@cern.ch>
2016-06-15 10:07:41 +0200

    update to v2

--- FlatTreeProducer/test/PROD/getSamples_MCRUN2.zsh
+++ FlatTreeProducer/test/PROD/getSamples_MCRUN2.zsh
@@ -1,6 +1,7 @@
 #!/bin/env zsh
 
-tag=RunIISpring16MiniAODv2-PUSpring16
+#tag=RunIISpring16MiniAODv2-PUSpring16
+tag=RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2
 
 wget --no-check-certificate \
 --output-document=samples_MCRUN2.txt \
