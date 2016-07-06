4197f1aef59324e2b4227f4bb130cf56b8d3a11a
Xavier COUBEZ <xavier.coubez@cern.ch>
2016-07-06 14:25:53 +0200

    Revert "Adding some tools for synchronization"
    This reverts commit fcfad537bc88e128d746af1f994df6933b83c12b.

--- FlatTreeProducer/python/ConfFile_MINIAOD_cfg.py
+++ FlatTreeProducer/python/ConfFile_MINIAOD_cfg.py
@@ -427,7 +427,7 @@ process.p = cms.Path(
                      process.METSignificance+
                      process.runQG+
 #                     process.HBHENoiseFilterResultProducer+
-                     process.BadChargedCandidateFilter+
-                     process.BadPFMuonFilter+
+#                     process.BadChargedCandidateFilter+
+#                     process.BadPFMuonFilter+
                      process.FlatTree
                     )
