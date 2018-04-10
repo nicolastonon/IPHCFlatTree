ebf692712a7cafeba613b7f720f6006e7ea106a3
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2018-04-10 10:05:11 +0200

    update weights

--- FlatTreeProducer/data/lepMVA/getFiles.zsh
+++ FlatTreeProducer/data/lepMVA/getFiles.zsh
@@ -1,19 +1,6 @@
 #!/bin/env zsh
 
-# use blob in the link
-wpath="https://github.com/CERN-PH-CMG/cmg-cmssw/blob/CMGTools-from-CMSSW_7_4_12/CMGTools/TTHAnalysis/data/leptonMVA/tth"
+wpath="https://raw.githubusercontent.com/peruzzim/cmgtools-lite/tree/94X_dev_M18_EGM17/TTHAnalysis/data/leptonMVA/tth"
 
-wget "${wpath}/el_eta_cb_BDTG.weights.xml?raw=true"
-mv el_eta_cb_BDTG.weights.xml\?raw\=true el_eta_cb_BDTG.weights.xml
-
-wget "${wpath}/el_eta_ec_BDTG.weights.xml?raw=true"
-mv el_eta_ec_BDTG.weights.xml\?raw\=true el_eta_ec_BDTG.weights.xml
-
-wget "${wpath}/el_eta_fb_BDTG.weights.xml?raw=true"
-mv el_eta_fb_BDTG.weights.xml\?raw\=true el_eta_fb_BDTG.weights.xml
-
-wget "${wpath}/mu_eta_b_BDTG.weights.xml?raw=true"
-mv mu_eta_b_BDTG.weights.xml\?raw\=true mu_eta_b_BDTG.weights.xml
-
-wget "${wpath}/mu_eta_e_BDTG.weights.xml?raw=true"
-mv mu_eta_e_BDTG.weights.xml\?raw\=true mu_eta_e_BDTG.weights.xml
+wget "${wpath}/el_BDTG.weights.xml"
+wget "${wpath}/mu_BDTG.weights.xml"
