920d0c59b1574d634434458cd4240e40cc8fd1f8
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2018-06-15 13:51:37 +0200

    update

--- FlatTreeProducer/test/PROD/getSamples_MC.zsh
+++ FlatTreeProducer/test/PROD/getSamples_MC.zsh
@@ -2,4 +2,4 @@
 
 tag=RunIIFall17MiniAOD-94X_mc2017_realistic_v10-v
 
-das_client --query="dataset dataset=/*/${tag}*/MINIAODSIM" --format=plain --limit=0 > samples_MC.txt
+das_client --query="dataset dataset=/*/${tag}*/MINIAODSIM status=*" --format=plain --limit=0 > samples_MC.txt
